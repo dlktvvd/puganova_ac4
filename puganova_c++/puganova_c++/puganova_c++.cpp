a5e5013beb6d9f40d5d3649e945ec5fe43eb9d8f
fgg-gg <alpuganova2016@gmail.com>
2025-10-01 21:54:53 +0300

    функции для ввода данных и главного меню

--- puganova_c++/puganova_c++/puganova_c++.cpp
+++ puganova_c++/puganova_c++/puganova_c++.cpp
@@ -20,3 +20,38 @@ struct CompressorStation {
     int working_workshops;
     int station_class;
 };
+
+// функция для безопасного ввода данных с проверкой корректности
+template <typename T>
+T get_valid_input(const string& prompt) {
+    T value;
+    while (true) {
+        cout << prompt;
+        cin >> value;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Please enter a valid value.\n";
+        }
+        else {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return value;
+        }
+    }
+}
+
+// функция отображения главного меню программы
+void display_main_menu() {
+    cout << "\n========================\n";
+    cout << "   PIPELINE SYSTEM MENU\n";
+    cout << "========================\n";
+    cout << "1. Add Pipe\n";
+    cout << "2. Add Compressor Station\n";
+    cout << "3. View All Objects\n";
+    cout << "4. Edit Pipe Repair Status\n";
+    cout << "5. Manage Station Workshops\n";
+    cout << "6. Save Data\n";
+    cout << "7. Load Data\n";
+    cout << "0. Exit\n";
+    cout << "========================\n";
+}
